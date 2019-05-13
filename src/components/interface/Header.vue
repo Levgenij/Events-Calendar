4cf23b3e9eb101fc9d54a146822d139a7d750de7
Eugene <ilevgenij@gmail.com>
2019-05-13 14:49:57 +0300

    Add header

---
+++ src/components/interface/Header.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-toolbar app class="app-toolbar" dark>
+		<v-toolbar-title>
+			<v-avatar color="grey" :size="30">
+				<img :src="user.avatar" alt="avatar">
+			</v-avatar>
+			{{user.name}}
+		</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<v-toolbar-items class="hidden-sm-and-down">
+			<v-btn flat></v-btn>
+		</v-toolbar-items>
+	</v-toolbar>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: 'Header',
+	computed: mapGetters(['user'])
+  }
+</script>
+
+<style scoped>
+
+</style>
